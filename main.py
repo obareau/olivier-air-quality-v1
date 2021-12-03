92acedd2736317b4205a5b7e9c0e3844c1ab472a
obareau <olivier.bareau@gmail.com>
2021-12-03 03:51:24 +0100

    python source updated

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ def on_button_pressed_a():
     kitronik_air_quality.clear()
     kitronik_air_quality.measure_data()
     kitronik_air_quality.show("Angie's Meteo Board")
-    kitronik_air_quality.show("Temperature - :" + str(kitronik_air_quality.read_temperature(kitronik_air_quality.TemperatureUnitList.C)) + " C",
+    kitronik_air_quality.show("Temperature - :" + ("" + str(kitronik_air_quality.read_temperature(kitronik_air_quality.TemperatureUnitList.C))) + " C",
         2)
-    kitronik_air_quality.show("Pressure ---- :" + str(kitronik_air_quality.read_pressure(kitronik_air_quality.PressureUnitList.PA)) + " Pa",
+    kitronik_air_quality.show("Pressure ---- :" + ("" + str(kitronik_air_quality.read_pressure(kitronik_air_quality.PressureUnitList.PA))) + " Pa",
         3)
-    kitronik_air_quality.show("Humidity ---- :" + str(kitronik_air_quality.read_humidity()) + " %",
+    kitronik_air_quality.show("Humidity ---- :" + ("" + str(kitronik_air_quality.read_humidity())) + " %",
         4)
-    kitronik_air_quality.show("IAQ Score --> " + str(kitronik_air_quality.get_air_quality_score()),
+    kitronik_air_quality.show("IAQ Score --> " + ("" + str(kitronik_air_quality.get_air_quality_score())),
         6)
-    kitronik_air_quality.show("IAQ %     --> " + str(kitronik_air_quality.get_air_quality_percent()),
+    kitronik_air_quality.show("IAQ %     --> " + ("" + str(kitronik_air_quality.get_air_quality_percent())),
         7)
-    kitronik_air_quality.show("eCo2      -->  " + str(kitronik_air_quality.reade_co2()), 8)
+    kitronik_air_quality.show("eCo2      -->  " + ("" + str(kitronik_air_quality.reade_co2())),
+        8)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
@@ -41,12 +42,12 @@ def on_gesture_shake():
     kitronik_air_quality.clear()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+basic.show_string("A for Meteo")
+basic.show_string("B for Logging Data")
+basic.show_string("A+B for Sending Data")
+# Date and Time are not really persistant and will be reseted to harcoded values on turn On
 kitronik_air_quality.set_date(2, 12, 21)
 kitronik_air_quality.set_time(22, 30, 0)
 kitronik_air_quality.add_project_info("Report", "Meteo-Nice")
 kitronik_air_quality.setup_gas_sensor()
 kitronik_air_quality.calc_baselines()
-
-def on_forever():
-    pass
-basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     kitronik_air_quality.clear()
     kitronik_air_quality.measureData()
     kitronik_air_quality.show("Angie's Meteo Board")
-    kitronik_air_quality.show("Temperature - :" + ("" + kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.C)) + " C", 2)
-    kitronik_air_quality.show("Pressure ---- :" + ("" + kitronik_air_quality.readPressure(kitronik_air_quality.PressureUnitList.Pa)) + " Pa", 3)
-    kitronik_air_quality.show("Humidity ---- :" + ("" + kitronik_air_quality.readHumidity()) + " %", 4)
-    kitronik_air_quality.show("IAQ Score --> " + ("" + kitronik_air_quality.getAirQualityScore()), 6)
-    kitronik_air_quality.show("IAQ %     --> " + ("" + kitronik_air_quality.getAirQualityPercent()), 7)
-    kitronik_air_quality.show("eCo2      -->  " + ("" + kitronik_air_quality.readeCO2()), 8)
+    kitronik_air_quality.show("Temperature - :" + ("" + ("" + kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.C))) + " C", 2)
+    kitronik_air_quality.show("Pressure ---- :" + ("" + ("" + kitronik_air_quality.readPressure(kitronik_air_quality.PressureUnitList.Pa))) + " Pa", 3)
+    kitronik_air_quality.show("Humidity ---- :" + ("" + ("" + kitronik_air_quality.readHumidity())) + " %", 4)
+    kitronik_air_quality.show("IAQ Score --> " + ("" + ("" + kitronik_air_quality.getAirQualityScore())), 6)
+    kitronik_air_quality.show("IAQ %     --> " + ("" + ("" + kitronik_air_quality.getAirQualityPercent())), 7)
+    kitronik_air_quality.show("eCo2      -->  " + ("" + ("" + kitronik_air_quality.readeCO2())), 8)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     kitronik_air_quality.sendAllData()
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     kitronik_air_quality.clear()
     kitronik_air_quality.show("Logging ...")
     for (let index = 0; index < 25; index++) {
@@ -25,7 +25,7 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(2000)
     kitronik_air_quality.clear()
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
     kitronik_air_quality.show("Erasing Memory", 4)
     kitronik_air_quality.eraseData()
     kitronik_air_quality.clearLine(4)
@@ -36,7 +36,7 @@ input.onGesture(Gesture.Shake, function () {
 basic.showString("A for Meteo")
 basic.showString("B for Logging Data")
 basic.showString("A+B for Sending Data")
-// Date and Time are not really persistant and will be reseted to harcoded values on turn On
+//  Date and Time are not really persistant and will be reseted to harcoded values on turn On
 kitronik_air_quality.setDate(2, 12, 21)
 kitronik_air_quality.setTime(22, 30, 0)
 kitronik_air_quality.addProjectInfo("Report", "Meteo-Nice")
