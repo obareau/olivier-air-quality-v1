14927c546b35788bfb0c12690c491b6529a51a3a
obareau <olivier.bareau@gmail.com>
2021-12-03 03:54:16 +0100

    Mise à jour pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     kitronik_air_quality.clear()
     kitronik_air_quality.measureData()
     kitronik_air_quality.show("Angie's Meteo Board")
-    kitronik_air_quality.show("Temperature - :" + ("" + ("" + kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.C))) + " C", 2)
-    kitronik_air_quality.show("Pressure ---- :" + ("" + ("" + kitronik_air_quality.readPressure(kitronik_air_quality.PressureUnitList.Pa))) + " Pa", 3)
-    kitronik_air_quality.show("Humidity ---- :" + ("" + ("" + kitronik_air_quality.readHumidity())) + " %", 4)
-    kitronik_air_quality.show("IAQ Score --> " + ("" + ("" + kitronik_air_quality.getAirQualityScore())), 6)
-    kitronik_air_quality.show("IAQ %     --> " + ("" + ("" + kitronik_air_quality.getAirQualityPercent())), 7)
-    kitronik_air_quality.show("eCo2      -->  " + ("" + ("" + kitronik_air_quality.readeCO2())), 8)
+    kitronik_air_quality.show("Temperature - :" + ("" + kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.C)) + " C", 2)
+    kitronik_air_quality.show("Pressure ---- :" + ("" + kitronik_air_quality.readPressure(kitronik_air_quality.PressureUnitList.Pa)) + " Pa", 3)
+    kitronik_air_quality.show("Humidity ---- :" + ("" + kitronik_air_quality.readHumidity()) + " %", 4)
+    kitronik_air_quality.show("IAQ Score --> " + ("" + kitronik_air_quality.getAirQualityScore()), 6)
+    kitronik_air_quality.show("IAQ %     --> " + ("" + kitronik_air_quality.getAirQualityPercent()), 7)
+    kitronik_air_quality.show("eCo2      -->  " + ("" + kitronik_air_quality.readeCO2()), 8)
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+input.onButtonPressed(Button.AB, function () {
     kitronik_air_quality.sendAllData()
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     kitronik_air_quality.clear()
     kitronik_air_quality.show("Logging ...")
     for (let index = 0; index < 25; index++) {
@@ -25,7 +25,7 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     basic.pause(2000)
     kitronik_air_quality.clear()
 })
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
+input.onGesture(Gesture.Shake, function () {
     kitronik_air_quality.show("Erasing Memory", 4)
     kitronik_air_quality.eraseData()
     kitronik_air_quality.clearLine(4)
@@ -36,7 +36,7 @@ input.onGesture(Gesture.Shake, function on_gesture_shake() {
 basic.showString("A for Meteo")
 basic.showString("B for Logging Data")
 basic.showString("A+B for Sending Data")
-//  Date and Time are not really persistant and will be reseted to harcoded values on turn On
+// Date and Time are not really persistant and will be reseted to harcoded values on turn On
 kitronik_air_quality.setDate(2, 12, 21)
 kitronik_air_quality.setTime(22, 30, 0)
 kitronik_air_quality.addProjectInfo("Report", "Meteo-Nice")
